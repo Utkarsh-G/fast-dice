50ae52952e304c58ad1c7b0dfc686fad90aff07f
Utkarsh Gaur <charmedtobe@gmail.com>
2022-12-28 22:09:52 -0500

    any click rolls

--- fast-dice/src/App.js
+++ fast-dice/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import RollDisplay from './RollDisplay';
+import { useState } from 'react';
 
 function App() {
+
+  const [roll, setroll] = useState(0)
+  function clickHandler(e) {
+    setroll(roll+1); // width: 100
+}
   return (
-    <div className="App">
+    <div className="App" onClick={clickHandler}>
+      {roll}
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
