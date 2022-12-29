5f56c03c9868e30f490d9fd5bc5d619053fbe5b4
Utkarsh Gaur <charmedtobe@gmail.com>
2022-12-28 22:03:35 -0500

    columns for all dice types

--- fast-dice/src/RollDisplay.js
+++ fast-dice/src/RollDisplay.js
@@ -4,10 +4,12 @@ function RollDisplay() {
     const [roll, setroll] = useState(0)
 
     function clickHandler(e) {
-        setroll(roll+1);
+        setroll(roll+1); // width: 100
     }
         return (
-        <div style={{margin: 'auto', width: 100, display: 'block'}}>
+        <div> 
+        <div style={{margin: 'auto', display: 'block', columnCount: 7}}>
+        <p> D20</p>
         <p> {Math.ceil(Math.random() * 20)} </p>
         <p> {Math.ceil(Math.random() * 20)} </p>
         <p> {Math.ceil(Math.random() * 20)} </p>
@@ -18,11 +20,109 @@ function RollDisplay() {
         <p> {Math.ceil(Math.random() * 20)} </p>
         <p> {Math.ceil(Math.random() * 20)} </p>
         <p> {Math.ceil(Math.random() * 20)} </p>
+
         <p>
         <button onClick={clickHandler}> Click </button>
         </p>
         
+
+        <p> D12</p>
+        <p> {Math.ceil(Math.random() * 12)} </p>
+        <p> {Math.ceil(Math.random() * 12)} </p>
+        <p> {Math.ceil(Math.random() * 12)} </p>
+        <p> {Math.ceil(Math.random() * 12)} </p>
+        <p> {Math.ceil(Math.random() * 12)} </p>
+        <p> {Math.ceil(Math.random() * 12)} </p>
+        <p> {Math.ceil(Math.random() * 12)} </p>
+        <p> {Math.ceil(Math.random() * 12)} </p>
+        <p> {Math.ceil(Math.random() * 12)} </p>
+        <p> {Math.ceil(Math.random() * 12)} </p>
+
+        <p>
+        <button onClick={clickHandler}> Click </button>
+        </p>
+
+        <p> D10</p>
+        <p> {Math.ceil(Math.random() * 10)} </p>
+        <p> {Math.ceil(Math.random() * 10)} </p>
+        <p> {Math.ceil(Math.random() * 10)} </p>
+        <p> {Math.ceil(Math.random() * 10)} </p>
+        <p> {Math.ceil(Math.random() * 10)} </p>
+        <p> {Math.ceil(Math.random() * 10)} </p>
+        <p> {Math.ceil(Math.random() * 10)} </p>
+        <p> {Math.ceil(Math.random() * 10)} </p>
+        <p> {Math.ceil(Math.random() * 10)} </p>
+        <p> {Math.ceil(Math.random() * 10)} </p>
         
+        <p>
+        <button onClick={clickHandler}> Click </button>
+        </p>
+
+        <p> D8</p>
+        <p> {Math.ceil(Math.random() * 8)} </p>
+        <p> {Math.ceil(Math.random() * 8)} </p>
+        <p> {Math.ceil(Math.random() * 8)} </p>
+        <p> {Math.ceil(Math.random() * 8)} </p>
+        <p> {Math.ceil(Math.random() * 8)} </p>
+        <p> {Math.ceil(Math.random() * 8)} </p>
+        <p> {Math.ceil(Math.random() * 8)} </p>
+        <p> {Math.ceil(Math.random() * 8)} </p>
+        <p> {Math.ceil(Math.random() * 8)} </p>
+        <p> {Math.ceil(Math.random() * 8)} </p>
+        
+        <p>
+        <button onClick={clickHandler}> Click </button>
+        </p>
+
+        <p> D6</p>
+        <p> {Math.ceil(Math.random() * 6)} </p>
+        <p> {Math.ceil(Math.random() * 6)} </p>
+        <p> {Math.ceil(Math.random() * 6)} </p>
+        <p> {Math.ceil(Math.random() * 6)} </p>
+        <p> {Math.ceil(Math.random() * 6)} </p>
+        <p> {Math.ceil(Math.random() * 6)} </p>
+        <p> {Math.ceil(Math.random() * 6)} </p>
+        <p> {Math.ceil(Math.random() * 6)} </p>
+        <p> {Math.ceil(Math.random() * 6)} </p>
+        <p> {Math.ceil(Math.random() * 6)} </p>
+        
+        <p>
+        <button onClick={clickHandler}> Click </button>
+        </p>
+
+        <p> D4</p>
+        <p> {Math.ceil(Math.random() * 4)} </p>
+        <p> {Math.ceil(Math.random() * 4)} </p>
+        <p> {Math.ceil(Math.random() * 4)} </p>
+        <p> {Math.ceil(Math.random() * 4)} </p>
+        <p> {Math.ceil(Math.random() * 4)} </p>
+        <p> {Math.ceil(Math.random() * 4)} </p>
+        <p> {Math.ceil(Math.random() * 4)} </p>
+        <p> {Math.ceil(Math.random() * 4)} </p>
+        <p> {Math.ceil(Math.random() * 4)} </p>
+        <p> {Math.ceil(Math.random() * 4)} </p>
+        
+        <p>
+        <button onClick={clickHandler}> Click </button>
+        </p>
+
+        <p> D100</p>
+        <p> {Math.ceil(Math.random() * 100)} </p>
+        <p> {Math.ceil(Math.random() * 100)} </p>
+        <p> {Math.ceil(Math.random() * 100)} </p>
+        <p> {Math.ceil(Math.random() * 100)} </p>
+        <p> {Math.ceil(Math.random() * 100)} </p>
+        <p> {Math.ceil(Math.random() * 100)} </p>
+        <p> {Math.ceil(Math.random() * 100)} </p>
+        <p> {Math.ceil(Math.random() * 100)} </p>
+        <p> {Math.ceil(Math.random() * 100)} </p>
+        <p> {Math.ceil(Math.random() * 100)} </p>
+        
+        <p>
+        <button onClick={clickHandler}> Click </button>
+        </p>
+        
+        </div>
         </div>
     );
   }
